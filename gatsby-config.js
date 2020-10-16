28475ccddc9660d65234045742c3bb09d5eb221f
Justin Johnson <justin@Justins-MacBook-Pro.local>
2020-10-15 18:06:15 -0700

    ?

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
+const proxy = require("http-proxy-middleware")
 module.exports = {
   /* Your site config here */
   developMiddleware: app => {
